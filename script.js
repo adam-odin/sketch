4b7cf1072c8bb9f310a2ce2950b51c5b287997d1
adam-odin <adam@edtec.io>
2024-09-09 20:21:25 +0100

    Fix grid buttons loop border

--- script.js
+++ script.js
@@ -1,44 +1,46 @@
-// console.log("Test: start of script.js file linked from html");
-
 const container = document.querySelector(".container");
 const maxSize = 960;
 let row;
 let square;
 
-let numberRows = 16;
-let numberColumns = numberRows;
+let numberRows=100;
+let numberColumns;
+let num=2;
+let proportion;
+let thisSquare;
 
 const buttonSet = document.querySelector(".setGrid");
 buttonSet.textContent="set grid size";
 const buttonClear = document.querySelector(".clearGrid");
 buttonClear.textContent="clear grid";
 
-buttonSet.addEventListener("click", () => {
-    clearGrid();
-    numberRows = prompt("how many squares per side?")
-    resize(numberRows);
-});
-
 function resize(num) {
     console.log("resize:" + num);
     
     numberRows = (num > maxSize ? maxSize : num);
     numberColumns=numberRows;
-    console.log(numberColumns);
+    proportion = 100/numberRows;
+    console.log("proportion: "+proportion);
+    console.log("numberColumns"+numberColumns);
 
-    for (let i = 1; i < numberRows+1; i++) {
+    for (let i = 0; i < numberRows; i++) {
         row = document.createElement("div");
         row.classList.add("row");
+        row.id="row"+(i+1);
+        console.log(row.id);
         container.appendChild(row);
-        for (let j = 1; j < numberColumns+1; j++) {
-            square = document.createElement("div");
-            square.classList.add("square");
-            square.classList.add("row"+i);
-            square.classList.add("col"+j);
-            square.id= "row" + i + "_col"+j;
 
-            if(j===numberRows) {square.classList.add("colLast")}; 
-            if(i===numberRows) {square.classList.add("rowLast")};
+        for (let j = 0; j < numberColumns; j++) {
+            square = document.createElement("div");
+            square.classList.add("square");            
+            thisSquare = document.getElementById("row"+(i+1)+"_col"+(j+1));
+            square.classList.add("row"+(i+1));
+            square.classList.add("col"+(j+1));
+            square.id= "row" + (i+1) + "_col"+(j+1);
+            square.style.height=proportion+"vw";
+            
+            if(i===(numberRows-1)) square.classList.add("rowLast");
+            if(j===(numberColumns-1)) square.classList.add("colLast"); 
 
             row.appendChild(square); 
 
@@ -59,18 +61,28 @@ function resize(num) {
     }
 };
 
-resize(numberRows);
-
-let thisSquare;
-
 function clearGrid() {
     console.log("clearGrid");
-    for (let i=1; i < numberRows+1; i++) {
-        for (let j=1; j < numberColumns+1; j++) {
-            thisSquare = document.getElementById("row"+i+"_col"+j);
+    for (let i=0; i < numberRows; i++) {
+        for (let j=0; j < numberColumns; j++) {
+            thisSquare = document.getElementById("row"+(i+1)+"_col"+(j+1));
             thisSquare.style.background="var(--hero)";
         }
     }
 };
 
 buttonClear.addEventListener("click", clearGrid);
+
+buttonSet.addEventListener("click", () => {
+    clearGrid();
+    const oldNumber = numberRows;
+    console.log("oldNumber: "+oldNumber);
+    newNumber = prompt("how many squares per side?")
+    console.log("newNumber: "+newNumber);
+    while(container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+    resize(newNumber);
+});
+
+resize(numberRows);
