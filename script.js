f566e63dd08df4a278d7b1fb9a9a91bc73ff038e
adam-odin <adam@edtec.io>
2024-09-09 19:14:12 +0100

    Set min width

--- script.js
+++ script.js
@@ -1,67 +1,76 @@
-console.log("Test: start of script.js file linked from html");
+// console.log("Test: start of script.js file linked from html");
 
-const body = document.querySelector("body");
 const container = document.querySelector(".container");
-let column;
+const maxSize = 960;
+let row;
 let square;
 
-const setGrid = document.querySelector(".setGrid");
-setGrid.textContent="set grid size";
-const clearGrid = document.querySelector(".clearGrid");
-clearGrid.textContent="clear grid";
+let numberRows = 16;
+let numberColumns = numberRows;
 
-// setGrid.addEventListener()
+const buttonSet = document.querySelector(".setGrid");
+buttonSet.textContent="set grid size";
+const buttonClear = document.querySelector(".clearGrid");
+buttonClear.textContent="clear grid";
 
-
-const numberRows = 16;
-const numberColumns = 16;
-
-for (let j = 1; j < numberColumns+1; j++) {
-    console.log("Column number (j):" + j);
-    column = document.createElement("div");
-    column.classList.add("column");
-    container.appendChild(column);
-    for (let i = 1; i < numberRows+1; i++) {
-        console.log("Column number (j):" + j);
-        square = document.createElement("div");
-        square.classList.add("square");
-        square.classList.add("row"+i);
-        square.classList.add("col"+j);
-        square.id= "row" + i + "_col"+j;
-
-        if (i===numberRows) {
-            square.classList.add("rowLast");
-        }
-        if(j===numberRows) {
-            square.classList.add("colLast");
-        }
-        column.appendChild(square); 
-
-        square.addEventListener("mouseenter", function(e) {
-            console.log("mouseenter");        
-            if(i=numberRows-1) {
-                square.classList.add("rowLast");
-            }
-            if(j=numberRows-1) {
-                square.classList.add("colLast");
-            }
-    
-            console.log(e.target);
-            e.target.style.background="var(--herod)";
-        });
-
-        square.addEventListener("mouseleave", function(e) {
-            console.log("mouseleave");
-            console.log(e.target);
-            e.target.style.background="var(--herot)";
-        });
-
-    } 
-}
-
-const squares = document.querySelector(".square");
-
-clearGrid.addEventListener("click", () => {
-    console.log("clearGrid");
-    container.style.background="var(--hero)";
+buttonSet.addEventListener("click", () => {
+    clearGrid();
+    numberRows = prompt("how many squares per side?")
+    resize(numberRows);
 });
+
+function resize(num) {
+    console.log("resize:" + num);
+    
+    numberRows = (num > maxSize ? maxSize : num);
+    numberColumns=numberRows;
+    console.log(numberColumns);
+
+    for (let i = 1; i < numberRows+1; i++) {
+        row = document.createElement("div");
+        row.classList.add("row");
+        container.appendChild(row);
+        for (let j = 1; j < numberColumns+1; j++) {
+            square = document.createElement("div");
+            square.classList.add("square");
+            square.classList.add("row"+i);
+            square.classList.add("col"+j);
+            square.id= "row" + i + "_col"+j;
+
+            if(j===numberRows) {square.classList.add("colLast")}; 
+            if(i===numberRows) {square.classList.add("rowLast")};
+
+            row.appendChild(square); 
+
+            square.addEventListener("mouseenter", function(e) {
+                console.log("mouseenter");        
+                console.log(e.target);
+                e.target.style.background="var(--herod)";
+            });
+
+            square.addEventListener("mouseleave", function(e) {
+                console.log("mouseleave");
+                console.log(e.target);
+                e.target.style.background="var(--herot)";
+            });
+
+        }
+        
+    }
+};
+
+resize(numberRows);
+
+let thisSquare;
+
+function clearGrid() {
+    console.log("clearGrid");
+    for (let i=1; i < numberRows+1; i++) {
+        for (let j=1; j < numberColumns+1; j++) {
+            thisSquare = document.getElementById("row"+i+"_col"+j);
+            thisSquare.style.background="var(--hero)";
+        }
+    }
+};
+
+buttonClear.addEventListener("click", clearGrid);
